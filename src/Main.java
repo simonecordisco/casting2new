3351d779d3a75a15c97e5315cac8a3718fae5715
simonecordisco <mnlsmncrd@gmail.com>
2023-11-02 14:53:48 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        char a='a';
+        char b='b';
+        int x=a;
+        int y=b;
+        System.out.println(x);
+        System.out.println(y);
+        int z=x+y;
+        System.out.println(z);
+    }
+}
